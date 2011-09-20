cc889219177a04edabb4ea2f43ba202d0eef972f
Brie <bbunge7@gmail.com>
2011-09-20 16:22:04 -0700

    unify conf

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once('config.secret.php');
+require_once(CONFPATH.'config.php');
 
 /*
 |--------------------------------------------------------------------------
